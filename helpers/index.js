548d9f88cb9a81044b87a71e352470ac89abe9c0
ksholovko <132094880+ksholovko@users.noreply.github.com>
2024-02-12 12:17:36 +0400

    6 - homework

--- helpers/index.js
+++ helpers/index.js
@@ -1,10 +1,12 @@
 const HttpError = require("./httpError");
 const handleMongooseError = require("./handleMongooseError");
 const ctrlWrapper = require("./ctrlWrapper");
+const sendEmail = require('./sendEmail')
 
 
 module.exports = {
     HttpError,
     handleMongooseError,
     ctrlWrapper,
+    sendEmail
 }
